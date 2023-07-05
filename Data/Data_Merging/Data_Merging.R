31cdfa2d63aba1570caab0d18af07c458a380b73
ethanhsu8004 <ethan.hsu@mail.utoronto.ca>
2023-07-05 00:50:38 -0400

    fixed data issue

--- Data/Data_Merging/Data_Merging.R
+++ Data/Data_Merging/Data_Merging.R
@@ -7,7 +7,11 @@ library(raster)
 
 Counties_Spatial <- st_read("../KenData/counties.shp")
 ZIP_Spatial <- st_read("../KenData/zips.shp")
-ZIP_valid <- subset(ZIP_Spatial, st_is_valid(ZIP_Spatial))
+ZIP_valid <- st_make_valid(ZIP_Spatial)
+
+# ZIP_valid <- subset(ZIP_Spatial, st_is_valid(ZIP_Spatial))
+
+
 # #------------------------------------------------------IGNORE------------------------------------#
 #Data did not involve cluster so ignore everything commented out.
 
@@ -81,6 +85,29 @@ ZIP_valid <- subset(ZIP_Spatial, st_is_valid(ZIP_Spatial))
 # 
 
 #------------------------------------------------------UPDATE------------------------------------#
+#Loading Ken's Data and joining it with Data_final_2016_2022
+Ken_2012_2020 <- readRDS("../KenData/vnf-2012-2020.rds")  
+
+#filtering out everything after 2016-01-03 inclusive
+Ken_2012_2016 <- Ken_2012_2020 %>% filter(!(date>= ("2016-01-03")), !(basin == "WILLISTON"))
+Ken_2012_2016$lat <- Ken_2012_2016$vnf_lat
+Ken_2012_2016$long <- Ken_2012_2016$vnf_lon
+Ken_2012_2016_coord <- st_as_sf(Ken_2012_2016, coords = c("long", "lat"), crs = 4326)
+Ken_2012_2016_zip_intersection <- st_join(Ken_2012_2016_coord, ZIP_valid) #adding the zip codes spatially joining
+Ken_2012_2016_zip_intersection$basin.y <- str_to_title(Ken_2012_2016_zip_intersection$basin.x)
+
+#Filtering out NA
+Ken_2012_2016_non_na <-  Ken_2012_2016_zip_intersection %>% filter(!is.na(Ken_2012_2016_zip_intersection$stusps)) %>%as.data.frame()
+#Updating State so that its based off the stusps column
+Ken_2012_2016_non_na$state <- ifelse(Ken_2012_2016_non_na$stusps == "NM", "New Mexico", "Texas")
+#updating column county so that it combines the county.x and stusps columns
+Ken_2012_2016_non_na$county.y <- paste(str_to_title(Ken_2012_2016_non_na$county.x), toupper(Ken_2012_2016_non_na$stusps), sep =", ")
+
+
+Ken_2012_2016_non_na <- Ken_2012_2016_non_na[,-ncol(Ken_2012_2016_non_na)] %>% rename(lat = "vnf_lat", long = "vnf_lon", county = "county.y", 
+                                                                                      basin = "basin.x")
+Ken_2012_2016_FINAL <-  Ken_2012_2016_non_na %>% dplyr::select("vnf_id", "date", "long", "lat", "zip", "state", "basin", "county")
+
 #After receiving Jerry's Updated Data
 Data_2016_2022 <- readRDS("../JerryData/flare_full.rds")
 Data_2016_2022$long <- Data_2016_2022$vnf_lon
@@ -91,37 +118,22 @@ Data_join_2016_2022 <- st_join(Data_join_2016_2022, ZIP_valid)
 
 
 #Filtering out temperatures and noise
-Data_filter_2016_2022 <- Data_join_2016_2022 %>% filter(temp_bb > 1600, cluster != 0)
+Data_filter_2016_2022 <- Data_join_2016_2022 %>% filter(temp_bb > 1600, cluster != 0) %>% filter(!(is.na(stusps)))
+Data_filter_2016_2022$state.y<- str_to_title(Data_filter_2016_2022$state.x)
+Data_filter_2016_2022$county.y <-  paste(str_to_title(Data_filter_2016_2022$county.x), toupper(Data_filter_2016_2022$stusps), sep =", ")
 
 #Filtering out NA
-Data_final_2016_2022 <- na.omit(Data_filter_2016_2022)
+# Data_final_2016_2022 <- na.omit(Data_filter_2016_2022)
 
 
-Data_final_2016_2022 <- Data_final_2016_2022 %>%st_transform(crs = 4326) %>%as.data.frame() 
+Data_final_2016_2022 <- Data_filter_2016_2022 %>%st_transform(crs = 4326) %>% as.data.frame() 
+
+Data_final_2016_2022$basin <-  Ken_2012_2016_non_na$basin.y[match(Data_final_2016_2022$county.y,Ken_2012_2016_non_na$county.y)]
 Data_final_2016_2022 <- Data_final_2016_2022 %>% rename(long = "vnf_lon", lat = "vnf_lat", county = "county.y", state = "state.y")
 Data_final_2016_2022 <- Data_final_2016_2022 %>% dplyr::select("vnf_id", "date", "long", "lat", "zip", "state", "basin", "county")
 
 
-#Loading Ken's Data and joining it with Data_final_2016_2022
-Ken_2012_2020 <- readRDS("../KenData/vnf-2012-2020.rds") 
-
-#filtering out everything after 2016-01-03 inclusive
-Ken_2012_2016 <- Ken_2012_2020 %>% filter(!(date>= ("2016-01-03")), !(basin == "WILLISTON"))
-Ken_2012_2016$lat <- Ken_2012_2016$vnf_lat
-Ken_2012_2016$long <- Ken_2012_2016$vnf_lon
-Ken_2012_2016_coord <- st_as_sf(Ken_2012_2016, coords = c("long", "lat"), crs = 4326)
-Ken_2012_2016_zip_intersection <- st_join(Ken_2012_2016_coord, ZIP_valid) #adding the zip codes spatially joining
-#Filtering out NA
-
-Ken_2012_2016_non_na <-  na.omit(Ken_2012_2016_zip_intersection) %>% as.data.frame()
-
-
-Ken_2012_2016_non_na <- Ken_2012_2016_non_na[,-ncol(Ken_2012_2016_non_na)] %>% rename(lat = "vnf_lat", long = "vnf_lon", county = "county.y", 
-                                                                                      basin = "basin.x")
-
-Ken_2012_2016_FINAL <-  Ken_2012_2016_non_na %>% dplyr::select("vnf_id", "date", "long", "lat", "zip", "state", "basin", "county")
-
-#Loading 2022(Sept) -2023
+#Loading 2022(Sept) - 2023
 Ethan_2022_2023 <- readRDS("VNF_2022-2023.rds")
 Ethan_2022_2023$vnf_lat <- Ethan_2022_2023$lat
 Ethan_2022_2023$vnf_lon <- Ethan_2022_2023$lon
