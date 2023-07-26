376ede1355218e98d95731784165f1ca18b412e2
ethanhsu8004 <133052726+ethanhsu8004@users.noreply.github.com>
2023-07-26 13:21:10 -0400

    changes that were supposed to be made

--- Data/Extracting/VNF-Process.R
+++ Data/Extracting/VNF-Process.R
@@ -6,10 +6,12 @@ rm(p, pkgs)
 
 
 #Loading Data
+
 PB_data <- readRDS("pb_vnf/pb_vnf.rds")
 EF_data <- readRDS("ef_vnf/ef_vnf.rds")
 
 #Loading Wells
+x <- readRDS("wells/pb_wells_prod.rds")
 PB_wells <- readRDS("wells/cleaned_wells_pb.rds") 
 EF_wells <- readRDS("Wells/cleaned_wells_ef.rds")
 
@@ -24,23 +26,29 @@ EF_data <- EF_data %>%  filter(temp_bb > 1600, cloud_mask == 0)
 EF_data <- EF_data %>% dplyr::select(-temp_bkg, -cloud_mask, -file_date)
 EF_data$basin <- "Western Gulf"
 
-
 #Combining both
 Data <- rbind(PB_data, EF_data) #Joining the two datasets
 
 
-# Data$uog_lat <- Data$lat
-# Data$uog_lon <- Data$lon
+Data$uog_lat <- Data$lat
+Data$uog_lon <- Data$lon
 
-# Data <- st_as_sf(Data, coords = c("uog_lon", "uog_lat"), crs = 4326)
-# 
-# 
+Data <- st_as_sf(Data, coords = c("uog_lon", "uog_lat"), crs = 4326)
+
 # #Merging the wells data
 # Wells_Merged <- rbind(PB_wells, EF_wells)
+# 
+# 
 # Wells_Merged$lat <- Wells_Merged$uog_lat
 # Wells_Merged$long <- Wells_Merged$uog_lon
-# Wells_Merged <- st_as_sf(Wells_Merged, coords = c("uog_lon", "uog_lat"), crs = 4326)
-# 
+# Wells_Merged <- st_as_sf(Wells_Merged, coords = c("uog_lon", "uog_lat"), crs = 4326) %>% st_transform(crs = 26914)
+
+# for(i in 1:50) {
+#   print(i)
+#   #run for loop inside if i want to filder by location
+#   if(length(st_is_within_distance(Data[i, ], Wells_Merged[1:1000,], dist = 1000)) > 0) {
+#     Data$well_within_1km[i] <- TRUE
+#   }}
 # 
 # Data_with_wells_distance <- st_join(Data, Wells_Merged, join = st_is_within_distance, dist = 1e3) 
 # Data_with_wells_distance <- Data_with_wells_distance %>% distinct(vnf_id, .keep_all = TRUE)
@@ -55,8 +63,9 @@ Data <- rbind(PB_data, EF_data) #Joining the two datasets
 # Data.clust.list <- Data_with_wells_distance %>% mutate(year = year(date)) 
 # Data.clust.list <- split(Data.clust.list,list(Data.clust.list$basin, Data.clust.list$year))
 
-Data.clust.list <- Data %>% mutate(year = year(date)) 
+Data.clust.list <- Data %>% mutate(year = year(date)) %>% as.data.frame()
 Data.clust.list <- split(Data.clust.list, list(Data.clust.list$basin, Data.clust.list$year))
+
 
 minpts.grid = 3:8 #the minpts used in HDBSCAN from 3-8 inclusive
 
@@ -114,8 +123,12 @@ for (i in 2:length(Data.clust.list)){
 }
 
 #Filter out Non clustered (cluster = 0)
-Output_Data <- Output_Data %>% filter(clustered != 0)
+Output_Data <- Output_Data %>% filter(value != 0)
 
-Final_Data <- merge(Output_Data, Data, by = "vnf_id")
+#saving locally to folder
+Final_Data <- merge(Data, Output_Data, by = "vnf_id")
 saveRDS(Final_Data, "VNF_2022-2023.rds")
+
+#saving into the data merging folder
+saveRDS(Final_Data, "../Data_Merging/VNF_2022-2023.rds")
 
